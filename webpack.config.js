d914b7414a18562287b499e5df590b322fb1b322
Ivan Jaimes <ivan.jaimes@iver.mx>
2020-04-18 11:24:40 -0500

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const devMode = process.env.NODE_ENV !== 'production';
 
@@ -11,6 +12,51 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   resolve: {
     extensions: ['.js', '.jsx'],
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i,
+        uglifyOptions: {
+          compress: {
+            arrows: false,
+            booleans: false,
+            cascade: false,
+            collapse_vars: false,
+            comparisons: false,
+            computed_props: false,
+            hoist_funs: false,
+            hoist_props: false,
+            hoist_vars: false,
+            if_return: false,
+            inline: false,
+            join_vars: false,
+            keep_infinity: true,
+            loops: false,
+            negate_iife: false,
+            properties: false,
+            reduce_funcs: false,
+            reduce_vars: false,
+            sequences: false,
+            side_effects: false,
+            switches: false,
+            top_retain: false,
+            toplevel: false,
+            typeofs: false,
+            unused: false,
+            // Switch off all types of compression except those needed to convince
+            // react-devtools that we're using a production build
+            conditionals: true,
+            dead_code: true,
+            evaluate: true,
+            cache: true,
+            sourceMap: true,
+            extractComments: true,
+          },
+          mangle: true,
+        },
+      }),
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -30,7 +76,7 @@ module.exports = {
       },
       {
         test: /\.(scss|sass|css)$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
         loaders: [
           MiniCssExtractPlugin.loader,
           {
